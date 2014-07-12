b59b86de19bce300575d687e905d3ce46df07af0
Steven Yang <aw0750@gmail.com>
2014-07-11 22:54:21 -0400

    Init

---
+++ Shuffle/Models/Deck.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
+using MongoDB.Driver.Linq;
+namespace Shuffle.Models
+{
+    public class Deck
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string[] deck { get; set; }
+        public string userID { get; set;}
+    }
+
+    
+
+    
+
+
+    public class MongoDBEntities : ApplicationDbContext
+    {
+
+    }
+}
